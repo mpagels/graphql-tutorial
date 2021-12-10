ff3143a269681e0d625cda7dcd0c432060834ee8
Martin <office@martinpagels.de>
2021-12-10 16:19:23 +0100

    🏷️ add root query in schema & express app

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
-app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({ schema }))
 
 app.listen(4000, () => {
   console.log('server is listening on port 4000')
