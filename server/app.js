921f0ab9464e15935e53751a49049fd618e3b23a
Martin <office@martinpagels.de>
2021-12-10 16:37:19 +0100

    🍱 activate graphiql

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const schema = require('./schema/schema')
 
 const app = express()
 
-app.use('/graphql', graphqlHTTP({ schema }))
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
 
 app.listen(4000, () => {
   console.log('server is listening on port 4000')
